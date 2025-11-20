d367a411628bddd82952c31600712d100719b3ff
Cường Trần <cuongtv50@viettel.com.vn>
2025-11-20 14:17:32 +0700

    Initial Commit

---
+++ Save4BBi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Save4BBi
+//
+//  Created by Cường Trần on 20/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Save4BBi/Save4BBiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Save4BBiApp.swift
+//  Save4BBi
+//
+//  Created by Cường Trần on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Save4BBiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
